2a7a0f477d1a75a8645c2d5b5560ec42b46dc37e
Haikal Azizan <mhaikalazizan@gmail.com>
2024-06-25 09:57:08 +0800

    Update hashing with 1 day validation period

--- uploader.py
+++ uploader.py
@@ -4,22 +4,26 @@ from flask import Flask, request, redirect, url_for, render_template_string, sen
 import os
 import hmac
 import hashlib
+from datetime import datetime
 
 app = Flask(__name__)
 UPLOAD_FOLDER = '/home/dev/files/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = 'abc12345'
 
-def generate_hash(filename, secret_key):
-    return hmac.new(secret_key.encode(), filename.encode(), hashlib.sha256).hexdigest()
+def generate_hash(filename, secret_key, date):
+    data = f"{filename}{date}{secret_key}"
+    return hashlib.sha256(data.encode()).hexdigest()
 
 # Ensure the upload folder exists
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 @app.route('/', methods=['GET'])
-def uploader():
-    return render_template_string(open('./uploader.html').read())
+def list_files():
+    #files = os.listdir(app.config['UPLOAD_FOLDER'])
+    #return jsonify(files=files)
+    return render_template_string(open('/home/dev/data/uploader.html').read())
 
 @app.route('/', methods=['POST'])
 def upload_file():
@@ -32,14 +36,16 @@ def upload_file():
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filepath)
         return f"OK\n"
-
+    
 @app.route('/d/<hashing>/<filename>')
 def uploaded_file(hashing, filename):
-    expected_hash = generate_hash(filename, app.config['SECRET_KEY'])[:8]
+    today_date = datetime.utcnow().strftime('%Y-%m-%d')
+    expected_hash = generate_hash(filename, app.config['SECRET_KEY'], today_date)[:8]
+    
     if hmac.compare_digest(hashing, expected_hash):
         return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
     else:
         abort(404)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=8004)
